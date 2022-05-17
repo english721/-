82b61fb5222bf9cef0d7d2f0e46fc0ba1324a312
english721 <english7212@gmail.com>
2022-05-17 16:24:38 +0900

    Revert "2022.05.16 강의 & 백준 3 완료"
    This reverts commit 9af3b83bc8eebcfafb9d51ee6606c87832e02039.

--- 3그리디/11399ATM.py
+++
@@ -1,8 +0,0 @@
-N = int(input())
-times = list(map(int, input().split()))
-answer = 0
-
-times.sort()
-for i in range(N):
-    answer += (N-i)*times[i]
-print(answer)
--- 3그리디/18310안테나.py
+++
@@ -1,6 +0,0 @@
-N = int(input())
-houses = list(map(int, input().split()))
-answer = 0
-houses.sort()
-answer = houses[(N-1)//2]
-print(answer)
